a8d6111840dbc00b016f69f4b7cdab380af214f7
takahiro-sawada <zeroshift.seed@gmail.com>
2020-08-13 20:23:58 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// BootstrapのJavaScript側の機能を読み込む
+import 'bootstrap';
+
+// スタイルシートを読み込む
+import './index.scss';
+
+// 拡張js
+import './extend.js';
---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const packageJson = require('./package.json');
+const version = packageJson.version;
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const AutoPrefixer = require('autoprefixer');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+
+    const conf = {
+        mode: 'development',
+        devServer: {
+            open: true,
+            openPage: 'index.html',
+            contentBase: path.join(__dirname, 'public'),
+            watchContentBase: true,
+            port: 8080,
+            host: argv.mode === 'production' ? `localhost` : `localhost`,// 0.0.0.0
+            disableHostCheck: true,
+        },
+        entry: {app: './src/index.js'},
+        output: {
+            path: path.join(__dirname, 'dist'),
+            publicPath: '/',
+            filename: argv.mode === 'production' ? `js/[name].js` : `js/[name].js`,  //`[name].min.js`
+        },
+
+        optimization: {
+            minimizer: [new TerserPlugin({
+                //extractComments: true,
+                //cache: true,
+                //parallel: true,
+                //sourceMap: true,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                }
+
+            })],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [
+                                    '@babel/env'
+                                ]
+                            }
+                        }
+                    ],
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: process.env.NODE_ENV === 'development',
+                            },
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    AutoPrefixer(
+                                        {
+                                            grid: 'autoplace'
+                                        },
+                                    ),
+                                ],
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                        }
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {loader: 'style-loader'},
+                        {loader: 'css-loader'},
+                    ]
+                },
+                {
+                    test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/i,
+                    use: [
+                        {loader: 'url-loader'},
+                    ]
+                },
+            ],
+        },
+        resolve: {
+            alias: {}
+        },
+        plugins: [
+            new webpack.BannerPlugin(`[name] v${version} Copyright (c) 2019 Your Name`),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            }),
+            new MiniCssExtractPlugin({
+                filename: argv.mode === 'production' ? `css/[name].css` : `css/[name].css`,  //`[name].min.js`
+            }),
+        ],
+
+    };
+
+    if (argv.mode !== 'production') {
+        conf.devtool = 'inline-source-map';
+    }
+
+    return conf;
+
+};
